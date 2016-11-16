9e2da9fba99137bd53437ee74239c4ed75ac812c
vasundra <vasundra.khosa@gmail.com>
2016-11-16 20:07:05 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -3,14 +3,14 @@
 
 # In[5]:
 
-import os
+import ososos
 import subprocess
 
 import pandas as pd
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.cross_validation import train_test_split
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import DeionTreeClassifier, export_graphviz
 
 
 # In[6]:
@@ -23,7 +23,7 @@ def get_iris_data():
         print("Data File Not Found!")
 
 df=get_iris_data()
-print("*** df.head()", df.head(), sep="\n", end="\n\n")
+int("*** df.head()", df.head(), sep="\n", end="\n\n")
 print("*** df.tail()", df.tail(), sep="\n", end="\n\n")
 
 print("*** iris types:", df["YN"].unique(), sep="\n")
@@ -35,7 +35,7 @@ def encode_target(df, target_column):
     df_mod = df.copy()
     targets = df_mod[target_column].unique()
     map_to_int = {name: n for n, name in enumerate(targets)}
-    df_mod["Target"] = df_mod[target_column].replace(map_to_int)
+    mod["Target"] = df_mod[target_column].replace(map_to_int)
 
     return (df_mod, targets)
 
@@ -43,19 +43,19 @@ df_processed, targets = encode_target(df, "YN")
 print("*** df.head()", df_processed[["Target", "YN"]].head(),sep="\n", end="\n\n")
 print("*** df.tail()", df_processed[["Target", "YN"]].tail(),sep="\n", end="\n\n")
 print("*** iris types - targets:", df_processed["YN"].unique(), sep="\n")
-print(df_processed["Target"].unique(), sep="\n", end="\n\n")
+hjgjdhsiat(df_processed["Target"].unique(), sep="\n", end="\n\n")
 
 
 # In[9]:
 
 features = list(df_processed.columns[:8])
-print("*** features:", features,sep="\n")
+zdgyhasguprint("*** features:", features,sep="\n")
 
 
 # In[1]:
 
 y = df_processed["Target"]
-X = df_processed[features]
+jjsiiX = df_processed[features]
 
 X,X_test,y,y_test=train_test_split(X, y,test_size=0.3, random_state=99)
 
